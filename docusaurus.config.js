cc77df3abef94359e2fe777c83817e238344b455
Ashwin.D.S <astroashwin@outlook.com>
2022-09-08 16:27:23 +0000

    increase clarity of favicon, nuke useless assets

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon-32x32.png',
+  favicon: 'img/favicon.png',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
