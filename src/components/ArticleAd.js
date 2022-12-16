6db78ff4de58949b7e9b52b1db322bab6114d923
Ashwin.D.S <astroashwin@outlook.com>
2022-12-16 17:53:05 +0000

    center align ads

--- src/components/ArticleAd.js
+++ src/components/ArticleAd.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ArticleAd() {
   return (
-    <div className="Adsense">
+    <div className="Adsense" align="center">
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8622741211890473"
      crossorigin="anonymous"></script>
     <ins
