8d52a81572f535751ae964d9c19cbe85784d00de
Ashwin.D.S <astroashwin@outlook.com>
2022-12-26 04:48:14 +0000

    man

--- src/components/InArticleAd.js
+++ src/components/InArticleAd.js
@@ -3,17 +3,7 @@ import React from 'react';
 export default function InArticleAd() {
   return (
     <div align="center">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8622741211890473"
-    crossorigin="anonymous"></script>
-    <ins class="adsbygoogle"
-    style="display:block; text-align:center;"
-    data-ad-layout="in-article"
-    data-ad-format="fluid"
-    data-ad-client="ca-pub-8622741211890473"
-    data-ad-slot="3686160267"></ins>
-    <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+      <ins className="adsbygoogle" style={{display: 'block', textAlign: 'center'}} data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-8622741211890473" data-ad-slot={3686160267} />
     </div>
   );
 }
