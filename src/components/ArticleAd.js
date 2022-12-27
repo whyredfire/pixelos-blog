5afefa420067073313da48847752de3e486ab686
Ashwin.D.S <astroashwin@outlook.com>
2022-12-27 08:36:36 +0000

    hrad

--- src/components/ArticleAd.js
+++ src/components/ArticleAd.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 export default function ArticleAd() {
   return (
-    <head>
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8622741211890473"
-     crossOrigin="anonymous"></script>
-    </head>
+     crossorigin="anonymous"></script>
   );
 }
