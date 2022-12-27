240cf926b182d2d70f40a6d3b33b662d23605b38
Ashwin.D.S <astroashwin@outlook.com>
2022-12-27 09:08:09 +0000

    rework

--- src/components/InArticleAd.js
+++ src/components/InArticleAd.js
@@ -2,19 +2,11 @@ import React from 'react';
 
 function InArticleAd() {
   return (
-    <>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8622741211890473"
-     crossorigin="anonymous"></script>
-    <ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-8622741211890473"
-     data-ad-slot="8167728112"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-    </>
+    <div>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8622741211890473" crossOrigin="anonymous"></script>
+      <ins className="adsbygoogle" style={{ display: 'block' }} data-ad-client="ca-pub-8622741211890473" data-ad-slot="8167728112" data-ad-format="auto" data-full-width-responsive="true"></ins>
+      <script>{`(adsbygoogle = window.adsbygoogle || []).push({});`}</script>
+    </div>
   );
 }
 
